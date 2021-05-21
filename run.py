bd26d2a073c5600da75ea9c509c36351f37b5a8d
thanh-pham2k <60598942+thanh-pham2k@users.noreply.github.com>
2021-05-21 10:44:12 +0700

    final work 1

--- run.py
+++ run.py
@@ -1,49 +1,5 @@
+from UtilityFunctions import *
 import argparse
-
-def log_func(func_name):
-    print("-------------------")
-    print(func_name)
-
-# Make sure the alphabet is an made of unique letters
-def validalpha(alphabet):
-    if len(set(alphabet)) != len(alphabet):
-        raise Exception("Alphabet cannot repeat any symbols")
-        
-# Plaintext must have specific form which we can check for.
-def validptext(T,alpha):
-    log_func("validptext")
-    if type(T) != str:
-        raise Exception("Plaintext must be a string")
-    
-    for i in T:
-        if i not in alpha:
-            raise Exception("{} is not a valid plaintext character".format(i))
-
-
-# Keys must also have specific form which we can check for.
-def validkeys(K,types):
-    
-    if type(types) != list:
-        if type(K) != types:
-            raise Exception("Key must be {}".format(types))
-    
-    
-    else:
-        for pos,pair in enumerate(zip(K,types),1):
-
-            if type(pair[0]) != pair[1]:
-                raise Exception("Key #{} must be {}".format(pos,pair[1]))
-
-# Convert letters to numbers according to some alphabet
-def alphaToNumber(L,alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-    return [alpha.index(i) for i in L]
-
-
-# Convert numbers to letters according to some alphabet
-def numberToAlpha(L,alpha="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
-    return [alpha[i] for i in L]
-
-
 def caesar(text,key,decode=False,alphabet="ABCDEFGHIJKLMNOPQRSTUVWXYZ"):
     log_func("caesar")
     text = text.upper()
